42fb176806f2f15767b8fff3bb33b9d2012bdc37
princend <waxdji3g4y3c04@gmail.com>
2024-08-21 23:34:17 +0800

    調整flask controller

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
-import os
 import cv2
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import joblib
-from keras.models import load_model
-from classify import classify
+from main import preprocessing_encode, recognize
 
 app = Flask(__name__)
 
+# 先載入將人臉特徵encode
 def load():
-    global face_model, svm_model, names
-    face_model = load_model(os.path.join('model','facenet_keras.h5'))
-    svm_model = joblib.load(os.path.join('model','svm_model.sav'))
-    names = np.load(os.path.join('model','classes.npy'))
+    global known_face_list, known_face_encodes
+    known_face_list,known_face_encodes=preprocessing_encode()
+    
 
 @app.route('/predict',methods=['POST'])
 def predict():
-
-
     filestr = request.files['image'].read()
-    npimg = np.fromstring(filestr, np.uint8)
+    npimg = np.frombuffer(filestr, np.uint8)
     image  = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-
-    pred = classify(image, face_model, svm_model)
-    return pred
+    result= recognize(image,known_face_list,known_face_encodes,tolerance=0.6)
+    return result
 
 @app.route('/help', methods=['GET'])
 def helpfunc():
     s = "I can recognize "
-    for i in range(len(names)):
-        if i==len(names)-1:
-                s += " and "+names[i]
-        else:
-                s += " "+names[i]+","
+    # 使用字典来去重
+    unique_data = {}
+    for item in known_face_list:
+        if item['name'] not in unique_data:
+            unique_data[item['name']] = item
 
+    # 转换回列表
+    filtered_list = list(unique_data.values())
+    for i in range(len(filtered_list)):
+        if i==len(filtered_list)-1:
+                s += " and "+filtered_list[i]['name']
+        else:
+                s += " "+filtered_list[i]['name']+","
     return s
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -9,46 +9,43 @@ from PIL import Image
 # 設定一個 tolerance，當最短距離超過此值時，我們認定無法辨識，回傳 unknown。
 tolerance = 0.6    
 
-# 读取JSON文件
-with open('singer-map.json', 'r',encoding='utf-8') as file:
-    known_face_list = json.load(file)
+def preprocessing_encode():
+    # 读取JSON文件
+    with open('singer-map.json', 'r',encoding='utf-8') as file:
+        known_face_list = json.load(file)
 
-# 将encode字段改为None
-for face in known_face_list:
-    face['encode'] = None
+    # 将encode字段改为None
+    for face in known_face_list:
+        face['encode'] = None
 
-# load image data by large model of face landmarks
-for data in known_face_list:
-    try:
-        img = cv2.imread('known_singer_image/'+data['filename'])
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        data['encode'] = face_recognition.face_encodings(img, model='small')[0]  # use small model of face landmarks
-    except Exception as error:
-        print('data is ',data,'error is ',error )
+    # load image data by large model of face landmarks
+    for data in known_face_list:
+        try:
+            img = cv2.imread('known_singer_image/'+data['filename'])
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            data['encode'] = face_recognition.face_encodings(img, model='small')[0]  # use small model of face landmarks
+        except Exception as error:
+            print('data is ',data,'error is ',error )
 
-# known_face_encodes = [data['encode'] for data in known_face_list]
+    known_face_encodes = [data['encode'] for data in known_face_list]
+    return known_face_list,known_face_encodes
     
-
+#辨認    
+def recognize(img_input,known_face_list,known_face_encodes,tolerance = 0.6)->str:
     
-# test_fn_list = ['lee-t1.jpg', 'lee-t2.jpg', 'pan-t1.jpg', 'pan-t2.jpg']
+        # img = cv2.imread(img_input)
+        img = cv2.cvtColor(img_input, cv2.COLOR_BGR2RGB)
 
+        cur_face_locs = face_recognition.face_locations(img)
+        cur_face_encodes = face_recognition.face_encodings(img, cur_face_locs, model='small')  # use small model of face landmarks
 
-# # face recognition
-# for fn in test_fn_list:
-#     img = cv2.imread(fn)
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
-#     cur_face_locs = face_recognition.face_locations(img)
-#     cur_face_encodes = face_recognition.face_encodings(img, cur_face_locs, model='small')  # use small model of face landmarks
-    
-#     for cur_face_encode in cur_face_encodes:
-#         face_distance_list = face_recognition.face_distance(known_face_encodes, cur_face_encode)
-        
-#         min_distance_index = np.argmin(face_distance_list)
-#         if face_distance_list[min_distance_index] < tolerance:
-#             result = known_face_list[min_distance_index]['name']
-#         else:
-#             result = 'unknown'
-            
-#         distance_with_name_list = [(face_data['name'], round(distance, 4)) for face_data, distance in zip(known_face_list, face_distance_list)]
-#         print(f'辨識檔案: {fn}, 辨識結果: {result}, 特徵距離: {distance_with_name_list}, 相差: {round(abs(distance_with_name_list[0][1] - distance_with_name_list[1][1]), 4)}')    
+        for cur_face_encode in cur_face_encodes:
+            face_distance_list = face_recognition.face_distance(known_face_encodes, cur_face_encode)
+            min_distance_index = np.argmin(face_distance_list)
+            if face_distance_list[min_distance_index] < tolerance:
+                result = known_face_list[min_distance_index]['name']
+            else:
+                result = 'unknown'
+            # distance_with_name_list = [(face_data['name'], round(distance, 4)) for face_data, distance in zip(known_face_list, face_distance_list)]
+            # print(f'辨識檔案: {img}, 辨識結果: {result}, 特徵距離: {distance_with_name_list}, 相差: {round(abs(distance_with_name_list[0][1] - distance_with_name_list[1][1]), 4)}')    
+            return result            
